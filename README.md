2a336e6a1ccdc3d80e4439dc2e2edf96ddabbe4f
Tianyu Wang <terrywang@tianyu.ninja>
2018-09-25 03:47:51 -0400

    Routers

---
+++ README.md
@@ -0,0 +1,2 @@
+# OAuth Passport
+* Learning OAuth following [YouTube Video](https://www.youtube.com/playlist?list=PL4cUxeGkcC9jdm7QX143aMLAqyM-jTZ2x)
--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 
+const { authRoutes } = require('./routes');
+
 const app = express();
 
 app.set('view engine', 'hbs');
+
+// setup routes
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.render('home');
